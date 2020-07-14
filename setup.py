32139a5fdaa5d23e9bde6f51f18073b11c5ca073
Michael Hall <mhall119@gmail.com>
2020-07-14 17:20:41 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,21 +5,21 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="django-totd", # Replace with your own username
-    version="1.0.1",
+    version="1.0.2",
     author="Michael Hall",
     author_email="mhall119@gmail.com",
     description="Django app for adding a Tip of the Day to any page",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/GetTogetherComm/totd",
+    url="https://github.com/GetTogetherComm/django-totd",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.5',
     install_requires=[
-        'Django>=2.0.12',
+        'Django>=2.2.0',
     ],
 )
